d51cd3cabeddbde84d036f99745c39db118f4a2c
Fabricio Werneck <fabricio.werneck@4linux.com.br>
2021-10-25 22:17:55 -0300

    ultima parte aula 5

--- aula5/ex_2.py
+++ aula5/ex_2.py
@@ -23,18 +23,75 @@ def salvar(dados, arquivo):
 def carregar(arquivo):
     return json.load(arquivo)
 
+def adicionar(regs):
+    regs['registros'].append(requisitar_dados())
+    return True
+
+def consultar(regs):
+    nome = input("Digite um nome para buscar: ").strip()
+
+    for reg in regs["registros"]:
+        if reg['nome'] == nome:
+            print(reg)
+            break
+
+    return False
+
+def remover(regs):
+    nome = input("Digite um nome para remover: ").strip()
+
+    for reg in regs["registros"]:
+        if reg['nome'] == nome:
+            regs['registros'].remove(reg)
+            return True
+
+    return False
+
 def main():
     nome_arquivo = 'ex_2.json'
 
     with open(nome_arquivo) as a:
         regs = carregar(a)
 
-    reg = requisitar_dados()
+    opcoes = {
+        '1': ('Adicionar registro', adicionar),
+        '2': ('Consultar', consultar),
+        '3': ('Remover registro', remover),
+        '-1': ('Finalizar', lambda: None)
+    }
 
-    regs['registros'].append(reg)
+    while True:
+        for (opcao, (descricao, _)) in opcoes.items():
+            print(f"{opcao} - {descricao}")
+        entrada = input("Escolha uma opção: ")
 
-    with open(nome_arquivo, 'w') as a:
-        salvar(regs, a)
+        if entrada == '-1':
+            break
+
+        # O código comentado abaixo substitui os próximos elif
+        # elif entrada in opcoes:
+        #     (_, fn) = opcoes[entrada]
+
+        #     if fn(regs):
+        #         with open(nome_arquivo, 'w') as a:
+        #             salvar(regs, a)
+
+        elif entrada == '1':
+            adicionar(regs)  # precisa salvar de qlq jeito
+            with open(nome_arquivo, 'w') as a:
+                salvar(regs, a)
+
+        elif entrada == '2':
+            consultar(regs)
+
+        elif entrada == '3':
+            deve_salvar = remover(regs)
+            if deve_salvar:  # descobre primeiro se precisa salvar
+                with open(nome_arquivo, 'w') as a:
+                    salvar(regs, a)
+
+        else:
+            print("Digite uma opção válida")
 
 if __name__ == '__main__':
     main()
